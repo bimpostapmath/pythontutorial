97e38f46a24ab79f774bf2ff916e1059fb985350
Leo Zipeng Lin <linzp@outlook.com>
2020-03-03 11:12:18 -0500

    Commit

---
+++ Chapter5_code/One_by_one.py
@@ -0,0 +1,8 @@
+Student1_name="Eric"
+Student1_Age=12
+Student2_name="Mary"
+Student2_Age=14
+# Start a report
+print(Student1_name+" "+str(Student1_Age))
+print(Student2_name+" "+str(Student2_Age))
+
